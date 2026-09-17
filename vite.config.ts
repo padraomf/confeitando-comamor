7dc3500ced7a9bf62f03d5ab5ce8f001230a0e89
Desenvolvedor <dev@confeitando.com>
2026-09-17 15:37:51 -0300

    Atualiza ID do banco D1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "10ff12e6-424e-487d-9d53-ac07fdcfac5d";
 
 const { d1, r2 } = hostingConfig;
 
